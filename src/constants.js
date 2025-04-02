28f5f4474f8914acf44f684b1937f3e362ee51e8
admin <chsh055@ess.ais.ac.nz>
2025-04-02 20:46:03 +1300

    delete todocard functionality working

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const BaseUrl = "https://to-do-app-backend-cyan.vercel.app";
-export const BaseUrl = "http://127.0.0.1:8000"; // Localhost backend
+export const BaseUrl = "https://to-do-app-backend-cyan.vercel.app";
+// export const BaseUrl = "http://127.0.0.1:8000"; // Localhost backend
