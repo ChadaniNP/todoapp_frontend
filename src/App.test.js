a1b17435b38c473caf22cbbee2acd1ed9859b38a
admin <chsh055@ess.ais.ac.nz>
2025-04-04 16:55:49 +1300

    updated test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navigation links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLink = screen.getByText(/home/i);
+  expect(homeLink).toBeInTheDocument();
 });
+
